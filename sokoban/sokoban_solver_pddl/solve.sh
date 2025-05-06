3eeb38231f429bd2f56628b64a0b9cde57bad5f6
Your Name <you@example.com>
2025-05-06 20:36:44 +0200

    fix sokoban

--- sokoban/sokoban_solver_pddl/solve.sh
+++ sokoban/sokoban_solver_pddl/solve.sh
@@ -18,14 +18,17 @@ if [ ! -f "$PROBLEM_FILE" ]; then
 fi
 
 echo "[Solver] Running solver with domain ${DOMAIN_FILE} and problem ${PROBLEM_FILE}..."
-output=$(java -cp ./pddl4j-4.0.0.0.jar -server -Xms2048m -Xmx2048m fr.uga.pddl4j.planners.statespace.HSP "$DOMAIN_FILE" "$PROBLEM_FILE" -t 1 -m 1000 | python3 parser/parse_output.py)
+output=$(java -cp ./pddl4j-4.0.0.jar -server -Xms2048m -Xmx2048m fr.uga.pddl4j.planners.statespace.HSP "$DOMAIN_FILE" "$PROBLEM_FILE" -t 1000 | python3 parser/parse_output.py)
 echo "[Solver] Output: ${output}"
-echo "[Viewer] Viewer launched go on https://localhost:8888 for visual solution"
 
-{
-    cd .. || exit 1
-    java --add-opens java.base/java.lang=ALL-UNNAMED \
-    -server -Xms2048m -Xmx2048m \
-    -cp "$(mvn dependency:build-classpath -Dmdep_outputFile=/dev/stdout -q):target/test-classes/:target/classes" \
-    schema.SokobanMain "$output" "$JSON_FILE" 
-}
+echo "[Viewer] Launching the viewing app, go on https://localhost:8888 to see the visual solution"
+
+(
+  cd .. || exit 1
+  java --add-opens java.base/java.lang=ALL-UNNAMED \
+       -server -Xms2048m -Xmx2048m \
+       -cp "$(mvn dependency:build-classpath -Dmdep.outputFile=/dev/stdout -q):target/test-classes/:target/classes" \
+       sokoban.SokobanMain "$output" "$JSON_FILE" > /dev/null 2>&1
+)
+
+echo "[Done] Viewer execution lancée discrètement."
